6db139eaee9c1eadf1cedade028405911f0ed1b1
linabiel <linuka.b@gmail.com>
2021-05-09 14:31:38 +0100

    added comments

--- js/app.js
+++ js/app.js
@@ -17,12 +17,11 @@ const handleFormSubmit = function (event) {
 
 const createArtistListItem = function (form) {
     const artistListItem = document.createElement('li');
-    artistListItem.classList.add('#artist-list-item');
+    artistListItem.classList.add('#artist-list-item');  // turns artistListItem into a list and asignes a class name to it 
 
     const artist = document.createElement('h2');
-    artist.textContent = form.artist.value;
-    artistListItem.appendChild(artist);
-
+    artist.textContent = form.artist.value;             // adds the newly created li(artistListItem) 
+    artistListItem.appendChild(artist);                 // to an ul(artistList) that we created above
     const field = document.createElement('p');
     field.textContent = form.field.value;
     artistListItem.appendChild(field);
